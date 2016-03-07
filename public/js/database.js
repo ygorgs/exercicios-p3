34395056ba692635c4c52c28b6846bc4ef6dfe1d
Ygor Santos <ygor.rodolfo.santos@ccc.ufcg.edu.br>
2016-03-07 12:37:58 -0300

    Add database

---
+++ public/js/database.js
@@ -0,0 +1,63 @@
+var db =[
+	{
+		titulo: "Jurassic Park",
+		autores: ["Michael Crichton"],
+		descricao: "Uma impressionante técnica de recuperação e clonagem de DNA de seres pré-históricos foi descoberta. Finalmente, uma das maiores fantasias da mente humana, algo que parecia impossível, tornou-se realidade. Agora, criaturas extintas há eras podem ser vistas de perto, para o fascínio e o encantamento do público. Até que algo sai do controle. Em Jurassic Park, escrito em 1990 por Michael Crichton, questões de bioética e a teoria do caos funcionam como pano de fundo para uma trama de aventura e luta pela sobrevivência. O livro inspirou o filme homônimo de 1993, dirigido por Steven Spielberg, uma das maiores bilheterias do cinema de todos os tempos.",
+		capa: "http://ecx.images-amazon.com/images/I/5197ws-6h1L._SX351_BO1,204,203,200_.jpg",
+		preco: 23.99
+	},{
+		titulo: "O Hobbit",
+		autores: ["J. R. R. Tolkien"],
+		descricao: "Os hobbits são seres muito pequenos, menores do que os anões. São de boa paz, sua única ambição é uma boa terra lavrada e só gostam de lidar com ferramentas manuais. Este livro tem como personagem central o hobbit Bilbo Bolseiro. Ele vive muito tranquilo até que o mago Gandalf e uma companhia de anões o levam numa expedição para resgatar um tesouro guardado por Smaug, um dragão enorme e perigoso.",
+		capa: "http://ecx.images-amazon.com/images/I/41KyAMWKYSL._SX295_BO1,204,203,200_.jpg",
+		preco: 26.99
+	},{
+		titulo: "Star Wars - A Trilogia",
+		autores: ["George Lucas", "Donald F. Glut", "James Kahn"],
+		descricao: "A saga que atravessou o espaço e inúmeras gerações de fãs retorna ao público brasileiro em grande estilo. As histórias clássicas de Luke Skywalker, Han Solo, Princesa Leia, Mestre Yoda e Darth Vader ganham as páginas luxuosas de Star Wars, A Trilogia. A obra reúne os romances inspirados nos três primeiros filmes do universo fantástico criado por George Lucas: Uma Nova Esperança, O Império Contra-Ataca e O Retorno de Jedi. Os três títulos chegaram a ser lançados no Brasil, sendo o último deles em 1983. Mas esta é a primeira vez que a trilogia completa é editada em nosso país num único volume, em capa dura.",
+		capa: "http://ecx.images-amazon.com/images/I/51MWO%2B1LhlL._SX361_BO1,204,203,200_.jpg",
+		preco: 36.90
+	},{
+		titulo: "O Iluminado",
+		autores: ["Stephen King"],
+		descricao: "Jack Torrence consegue um emprego de zelador em um velho hotel, e acha que será a solução dos problemas de sua família - não vão mais passar por dificuldades, sua esposa não vai mais sofrer e seu filho, Danny, vai poder ter ar puro para se livrar de estranhas convulsões. Mas as coisas não são tão perfeitas como parecem - existem forças malignas rondando os antigos corredores. O hotel é uma chaga aberta de ressentimento e desejo de vingança, e, inevitavelmente, um embate entre o bem e o mal terá de ser travado.",
+		capa: "http://ecx.images-amazon.com/images/I/51dU%2Bs4FLDL._SX346_BO1,204,203,200_.jpg",
+		preco: 29.10
+	},{
+		titulo: "Eu Sou a Lenda",
+		autores: ["Richard Matheson"],
+		descricao: "Uma impiedosa praga assola o mundo, transformando cada homem, mulher e criança do planeta em algo digno dos pesadelos mais sombrios. Nesse cenário pós-apocalíptico, tomado por criaturas da noite sedentas de sangue, Robert Neville pode ser o último homem na Terra. Ele passa seus dias em busca de comida e suprimentos, lutando para manter-se vivo (e são). Mas os infectados espreitam pelas sombras, observando até o menor de seus movimentos, à espera de qualquer passo em falso... Eu sou a lenda, é considerado um dos maiores clássicos do horror e da ficção científica, tendo sido adaptado para o cinema três vezes."
+		capa: "http://ecx.images-amazon.com/images/I/51xxDnZu6FL._SX332_BO1,204,203,200_.jpg",
+		preco: 30.50
+	}, {
+		titulo: "As Crônicas de Nárnia",
+		autores: ["C. S Lewis"],
+		descricao: "Viagens ao fim do mundo, criaturas fantásticas e batalhas épicas entre o bem e o mal - o que mais um leitor poderia querer de um livro? O livro que tem tudo isso é O leão, a feiticeira e o guarda-roupa, escrito em 1949 por Clive Staples Lewis. Mas Lewis não parou por aí, seis outros livros vieram depois e, juntos, ficaram conhecidos como As crônicas de Nárnia. Nos últimos cinquenta anos, As crônicas de Nárnia transcenderam o gênero da fantasia para se tornar parte do cânone da literatura clássica. Cada um dos sete livros é uma obra-prima, atraindo o leitor para um mundo em que a magia encontra a realidade, e o resultado é um mundo ficcional que tem fascinado gerações. Esta edição apresenta todas as sete crônicas integralmente, num único volume magnífico. Os livros são apresentados de acordo com a ordem de preferência de Lewis, cada capítulo com uma ilustração do artista Pauline Baynes. Enganosamente simples e diretas, As crônicas de Nárnia continuam cativando os leitores com aventuras, personagens e fatos que falam a pessoas de todas as idades. Esta edição contém também o ensaio 'Três maneiras de escrever para crianças'."
+		capa: "http://ecx.images-amazon.com/images/I/51lwu3FTjGL._SX331_BO1,204,203,200_.jpg",
+		preco: 29.90
+	}, {
+		titulo: "Fortaleza Digital",
+		autores: ["Dan Brown"],
+		descricao: "Em Fortaleza Digital, Dan Brown mergulha no intrigante universo dos serviços de informação e ambienta sua história na ultrassecreta e multibilionária NSA, a Agência de Segurança Nacional americana, mais poderosa do que a CIA ou qualquer outra organização de inteligência do mundo."
+		capa: "http://ecx.images-amazon.com/images/I/512xTwg97dL._SX326_BO1,204,203,200_.jpg",
+		preco: 12.45
+	}, {
+		titulo: "O Cão dos Baskerville",
+		autores: ["Arthur Conan Doyle"],
+		descricao: "O detetive Sherlock Holmes e seu fiel amigo dr. Watson retornam em mais uma série de mistérios, assassinatos, pistas e perseguições em O cão dos Baskerville. Considerada uma das melhores aventuras do detetive, a obra foi originalmente impressa em partes, publicadas pela revista Strand Magazine de agosto de 1901 a abril de 1902, quando, no mesmo ano, foi lançada em formato de livro. O mistério a ser solucionado envolve a morte de Sir Charles Baskerville e um cão fantasmagórico que aterroriza os moradores da mansão Baskerville Hall. O cão dos Baskerville possui uma história eletrizante que se tornou um clássico da literatura policial e promete prender a atenção do leitor a cada virada de página."
+		capa: "http://ecx.images-amazon.com/images/I/51KwSIimjhL._SX315_BO1,204,203,200_.jpg",
+		preco: 20.99
+	}, {
+		titulo: "Drácula",
+		autores: ["Bram Stoker"],
+		descricao: "O jovem advogado inglês Jonathan Harker é enviado por seu patrão à Transilvânia, no leste da Europa, para atender um misterioso cliente. Ao chegar, descobre que seu anfitrião é, na realidade, um poderoso vampiro - o conde Drácula - que pretende conquistar o mundo transferindo-se para a Inglaterra, onde será combatido por um grupo de corajosos cavalheiros."
+		capa: "http://ecx.images-amazon.com/images/I/517qeYna7HL._SX346_BO1,204,203,200_.jpg",
+		preco: 20.30
+	}, {
+		titulo: "O Pequeno Príncipe",
+		autores: [" Antonie de Saint-Exupéry"],
+		descricao: "Um piloto cai com seu avião no deserto e ali encontra uma criança loura e frágil. Ela diz ter vindo de um pequeno planeta distante. E ali, na convivência com o piloto perdido, os dois repensam os seus valores e encontram o sentido da vida. Com essa história mágica, sensível, comovente, às vezes triste, e só aparentemente infantil, o escritor francês Antoine de Saint-Exupéry criou há 70 anos um dos maiores clássicos da literatura universal. Não há adulto que não se comova ao se lembrar de quando o leu quando criança. Trata-se da maior obra existencialista do século XX, segundo Martin Heidegger. Livro mais traduzido da história, depois do Alcorão e da Bíblia, ele agora chega ao Brasil em nova edição, completa, com a tradução de Frei Betto e enriquecida com um caderno ilustrado sobre a obra e a curta e trágica vida do autor."
+		capa: "http://ecx.images-amazon.com/images/I/516xay6urHL._SX341_BO1,204,203,200_.jpg",
+		preco: 17,19
+	}	
+]
